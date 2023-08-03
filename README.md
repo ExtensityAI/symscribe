0507b8cf8fb0a0164774012ca72a5e9a3d4c3113
leoentersthevoid <c.leov@icloud.com>
2023-08-03 14:48:38 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # symscribe
-Whisper-based transcription tool with chapter segmentation and timestamp handling.
+A repository for transcribing audio files using Whisper. It provides guidelines for creating chapters at logical topic transitions, with concise headings and respect for timestamps. It performs transcription and export the results as `chapters.csv` and `transcript.csv` files.
+
+## Installation
+1. Install `symbolicai`
+```bash
+pip install symbolicai[whisper]
+```
+2. Use the builtin `sympkg` to install the package
+```bash
+sympkg i ExtensityAI/symscribe
+```
+3. Create an alias for the `symscribe` command
+```bash
+symrun c symscribe ExtensityAI/symscribe
+```
+## Usage
+Supported features:
+- Whisper models: all models supported by `symbolicai` (default: `"model=whisper:en:base"`)
+  - To change the model, before you run the `symscribe` do `export SPEECH_ENGINE_MODEL="..."`, where `...` is the model name ([available](https://github.com/openai/whisper#available-models-and-languages).
+- Language: `"language=..."` (default: `"language=en"`)
+- Export directory: `"export_dir=..."` (default: `"export_dir=."`)
+- Bin size: `"bin_size_s=..."` (default: `"bin_size_s=300"`)
+  - The bin size is the duration of each audio file in seconds when splitting the audio file into smaller chunks.
+
+Example:
+```bash
+symrun symscribe "path_to_file.mp3/mp4/..." "language=en" "export_dir=/tmp" "bin_size=300"
+
---
+++ src/func.py
@@ -0,0 +1,54 @@
+import re
+from pathlib import Path
+
+import pandas as pd
+from symai import Expression, Function, Interface
+
+FUNCTION_DESCRIPTION = '''
+Transcription Guidelines:
+    Create new chapters at logical topic transitions.
+    Highlight significant events or discussions.
+    Keep chapter headings concise, use less than 2 words.
+    Respect the timestamps.
+    Use commas to separate multiple topics.
+    Use a period at the end of each chapter.
+    Use a blank line between chapters.
+    Don't add quotes or other special characters other than the ones required by the template.
+    Template:
+        HH:MM:SS - {comma separated headings}.\n
+'''
+
+class MyExpression(Expression):
+    def __init__(self):
+        super().__init__()
+        self.fn = Function(FUNCTION_DESCRIPTION)
+
+    def forward(self, data, template: str = '', *args, **kwargs):
+        lang = kwargs.get("language", "en")
+        bin_size_s = int(kwargs.get("bin_size_s", 5 * 60)) #@NOTE: default to 5 minutes bins
+        export_path = kwargs.get("export_path", Path.cwd())
+        whisper = Interface("whisper")
+        transcript = whisper(data, language=lang, word_timestamps=True, disable_pbar=True)
+        bins = self.get_bins(transcript.value, bin_size_s)
+        chapters = "\n".join([self.fn(bin).value for bin in bins])
+        pd.DataFrame(chapters.split("\n"), columns=["Chapters"]).dropna().to_csv(Path(export_path) / "chapters.csv")
+        pd.DataFrame(transcript.value.split("\n"), columns=["Transcript"]).dropna().to_csv(Path(export_path) / "transcript.csv")
+        return f"Files were successfully exported to {export_path} as chapters.csv and transcript.csv"
+
+    def get_bins(self, data: str, bin_size_s: int) -> str:
+        tmps = map(self.seconds, re.findall(r"\b\d{2}:\d{2}:\d{2}\b", data))
+        data = zip(tmps, data.split("\n"))
+        bin = []
+        for tmp, seg in data:
+            bin.append(seg)
+            if tmp == 0 or (tmp - bin_size_s) % bin_size_s != 0:
+                continue
+            yield "\n".join(bin)
+            bin = []
+        yield "\n".join(bin)
+        bin = []
+
+    def seconds(self, tmp: str) -> int:
+        h, m ,s = tmp.split(":")
+        return int(h) * 3600 + int(m) * 60 + int(s)
+
